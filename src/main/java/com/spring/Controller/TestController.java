633d358e8f03e3c8499a6f7ea4825049abf95cd8
Arabira <447625686@qq.com>
2016-09-19 22:25:57 +0800

    添加mysql配置，添加json数据传输

--- src/main/java/com/spring/Controller/TestController.java
+++ src/main/java/com/spring/Controller/TestController.java
@@ -2,6 +2,10 @@ package com.spring.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Arabira on 2016/9/8.
@@ -11,5 +15,13 @@ public class TestController {
     @RequestMapping("/test")
     public String test() {
         return "success";
+    }
+
+    @ResponseBody
+    @RequestMapping("/return")
+    public Map<String, String> test2() {
+        Map<String, String > map = new HashMap<String, String>();
+        map.put("status", "ok");
+        return map;
     }
 }
